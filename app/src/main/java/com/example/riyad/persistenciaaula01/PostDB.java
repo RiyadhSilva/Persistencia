3e81aa99ee2a86b6b7e02e52d8a93446064b1bb5
RiyadhSilva <riyadhlevi@gmail.com>
2016-08-17 16:11:19 -0300

    Criação do método save
    Agora é possível salvar no banco.

--- app/src/main/java/com/example/riyad/persistenciaaula01/PostDB.java
+++ app/src/main/java/com/example/riyad/persistenciaaula01/PostDB.java
@@ -1,5 +1,6 @@
 package com.example.riyad.persistenciaaula01;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -24,6 +25,38 @@ public class PostDB extends SQLiteOpenHelper {
         "_id integer primary key autoincrement, " +
         " texto text, likes integer, data data);");
         Log.d(TAG, "Tabela carro criada com sucesso.");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public long save(Post post){
+        long id = post.getId();
+        SQLiteDatabase db = getWritableDatabase();
+        try{
+            ContentValues values = new ContentValues();
+            values.put("texto", post.getTexto());
+            values.put("likes", post.getLikes());
+            values.put("data", post.getData().getTime());
+            if( id != 0){
+                String _id = String.valueOf(post.getId());
+                String[] whereArgs = new String[]{_id};
+                int count = db.update("post", values," _id=?", whereArgs);
+                Log.i(TAG, "Atualizou post");
+                return count;
+            } else {
+                id = db.insert("post", "", values);
+                Log.d(TAG, "Inseriu post");
+                return id;
+            }
+        }catch (Exception e){
+            Log.d(TAG, "Erro: " + e.getMessage());
+            return 0;
+        } finally {
+            db.close();
+        }
     }
 
 
